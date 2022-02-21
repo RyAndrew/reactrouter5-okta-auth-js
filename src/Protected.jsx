6cd007282b0b94c4330f2b0a7b08a73ae261b9d4
RyAndrew <RyAndrew@users.noreply.github.com>
2022-02-21 10:59:23 -0600

    on protected page show logged in user name

--- src/Protected.jsx
+++ src/Protected.jsx
@@ -1,4 +1,17 @@
 import React from 'react';
 
-const Home = () => <h3>Protected</h3>;
+import { useOktaAuth } from '@okta/okta-react';
+
+const Home = () => {
+  
+  const { authState, oktaAuth } = useOktaAuth();
+  let name;
+  if(authState.isAuthenticated){
+    name = 'Logged In As '+authState.idToken.claims.name;
+  }
+  
+  return <div>
+    <h3>Protected</h3><p>{name}</p>
+  </div>;
+}
 export default Home;
