46df7f1dcb9e602768a6bb4957d6423cba21dd69
RyAndrew <RyAndrew@users.noreply.github.com>
2022-02-21 10:43:54 -0600

    use webfinger to discover IDP and do a redirect based login

--- src/SignInForm.jsx
+++ src/SignInForm.jsx
@@ -1,23 +1,61 @@
 import React, { useState } from 'react';
 import { useOktaAuth } from '@okta/okta-react';
 
+
+
 const SignInForm = () => {
   const { oktaAuth } = useOktaAuth();
   const [sessionToken, setSessionToken] = useState();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    oktaAuth.signInWithCredentials({ username, password })
+  const loginViaRedirect = (idp) => {
+    let tokenParams = {
+      idp: idp,
+      scopes: ['openid', 'email', 'profile'],
+    }
+    oktaAuth.token.getWithRedirect(tokenParams);
+  }
+  
+  const loginWithCredentials = (username, password) => {
+      oktaAuth.signInWithCredentials({ username, password })
     .then(res => {
       const sessionToken = res.sessionToken;
       setSessionToken(sessionToken);
       // sessionToken is a one-use token, so make sure this is only called once
       oktaAuth.signInWithRedirect({ sessionToken });
     })
-    .catch(err => console.log('Found an error', err));
+    .catch(err => {
+      console.log('Found an error', err)
+    });
+  };
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    oktaAuth.webfinger({
+      resource: 'acct:'+username,
+      rel: 'okta:idp'
+    })
+    .then(function(response) {
+      // use the webfinger response to redirect to idp for login if necessary
+      console.log(response);
+
+      if(response.links && response.links[0] && response.links[0].rel){
+        if(response.links[0].rel === 'okta:idp'){
+          loginViaRedirect(response.links[0].properties["okta:idp:id"]);
+        }else{
+          loginWithCredentials(username, password);
+        }
+      }else{
+        console.error('invalid webfinger response', response);
+      }
+    })
+    .catch(function(err) {
+      console.error('webfinger error',err);
+    });
+    
+
   };
 
   const handleUsernameChange = (e) => {
@@ -35,7 +73,7 @@ const SignInForm = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <p>Login with Okta user</p>
+      <h2>Login</h2>
       <label>
         Username:
         <input
